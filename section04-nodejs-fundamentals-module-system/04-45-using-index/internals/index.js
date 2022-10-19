f186263fa0df4991efbac075ae93c356f1e0c49e
OpenTech-Consult <opentech.consult@gmail.com>
2022-10-19 11:57:01 +0000

    Another alternative way of using the index.js file using the spread operator.

--- section04-nodejs-fundamentals-module-system/04-45-using-index/internals/index.js
+++ section04-nodejs-fundamentals-module-system/04-45-using-index/internals/index.js
@@ -1,8 +1,6 @@
-const request = require('./request.js')
-const response = require('./response.js')
+
 
 module.exports = {
-    REQUEST_TIMEOUT: request.REQUEST_TIMEOUT,
-    send: request.send,
-    read: response.read,
+    ...require('./request'),
+    ...require('./response'),
 }
